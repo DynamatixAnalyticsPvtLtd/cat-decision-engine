13de1106aa5cdee84a616382f6ac76df2dbf86c8
27Shri03 <shrirambhardwaj68@gmail.com>
2025-05-17 18:03:17 +0530

    (refactor): Moved index.ts in root directory

---
+++ index.ts
@@ -0,0 +1,3 @@
+// Core exports
+export { WorkflowAlertModel } from './src/features/alert/models/alert.model';
+export { WorkflowMethod } from './src/core/decorators/workflow-method.decorator';
--- src/index.ts
+++
@@ -1,4 +0,0 @@
-// Core exports
-export { WorkflowAlertModel } from './features/alert/models/alert.model';
-export { WorkflowMethod } from './core/decorators/workflow-method.decorator';
-
